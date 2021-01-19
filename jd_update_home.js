a8330f93e58f963c6d8ba5ce1f800f0b9b00f923
952714408 <46145766+952714408@users.noreply.github.com>
2021-01-20 02:36:41 +0800

    Update jd_update_home.js

--- jd_update_home.js
+++ jd_update_home.js
@@ -64,7 +64,7 @@ function showMsg() {
   return new Promise(async resolve => {
     console.log($.shareCode)
     try {
-      await $.http.get({url: `https://purge.jsdelivr.net/gh/lxk0301/updateTeam@master/jd_updateSmallHomeInviteCode.json`}).then((resp) => {
+      await $.http.get({url: `https://purge.jsdelivr.net/gh/952714408/updateTeam@master/jd_updateSmallHomeInviteCode.json`}).then((resp) => {
         if (resp.statusCode === 200) {
           console.log(`已刷新CDN缓存`)
         } else {
